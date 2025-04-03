25ec9704fdbc52b7ee21068ff4febb781164ba1d
Jeremy Harris <jeremy.harris@asirobots.com>
2025-04-03 15:48:12 -0600

    Update Class1.cs

--- LaughingJourney/Projects/Plugins/Yardshifting/ClassLibrary4/Class1.cs
+++ LaughingJourney/Projects/Plugins/Yardshifting/ClassLibrary4/Class1.cs
@@ -5,4 +5,5 @@ public class Class1
     public int Number { get; set; } = 5;
     public float Noomber { get; } = 2.0f;
     public string Streeng {get;} = "A Streeng";
+    public bool Bool { get; } = false;
 }
